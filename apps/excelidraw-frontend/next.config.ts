3beeae4c498e043199d48160ccdd21b589c9349e
Nitin Kumar <143751900+nitin1kum@users.noreply.github.com>
2025-11-12 22:41:04 +0530

    update vercel build 1

--- apps/excelidraw-frontend/next.config.ts
+++ apps/excelidraw-frontend/next.config.ts
@@ -4,15 +4,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        destination: "https://anydraw-frontend.onrender.com/:path*",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
